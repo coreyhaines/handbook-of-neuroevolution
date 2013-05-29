df4b1e517fd03a2f0a53cc0663f79ed16062571d
coreyhaines <coreyhaines@gmail.com>
2013-05-28 20:50:10 -0500

    Elixir simplest neural network

---
+++ elixir/ch6/simplest_nn.ex
@@ -0,0 +1,70 @@
+defmodule SimplestNN do
+  def create do
+    weights = [:random.uniform() - 0.5, :random.uniform() - 0.5, :random.uniform() - 0.5]
+    n_pid = Process.spawn(__MODULE__, :neuron, [weights, nil, nil])
+    s_pid = Process.spawn(__MODULE__, :sensor, [n_pid])
+    a_pid = Process.spawn(__MODULE__, :actuator, [n_pid])
+    n_pid <- {:init, s_pid, a_pid}
+    Process.register(Process.spawn(__MODULE__, :cortex, [s_pid, n_pid, a_pid]), :cortex)
+  end
+
+  def neuron(weights, s_pid, a_pid) do
+    receive do
+      {s_pid, :forward, input} ->
+        :io.format("****Thinking****~nInput:~p~nwith Weights:~p~n", [input, weights])
+        dot_product = dot(input, weights, 0)
+        output = [:math.tanh(dot_product)]
+        a_pid <- {self(), :forward, output}
+        neuron(weights, s_pid, a_pid)
+      {:init, new_spid, new_apid} ->
+        neuron(weights, new_spid, new_apid)
+      :terminate ->
+        :ok
+    end
+  end
+
+  def dot([i|input],[w|weights],acc) do
+    dot(input, weights, i*w+acc)
+  end
+  def dot([], [], acc) do
+    acc
+  end
+  def dot([], [bias], acc) do
+    acc + bias
+  end
+
+  def sensor(n_pid) do
+    receive do
+      :sync ->
+        sensory_signal = [:random.uniform(), :random.uniform()]
+        :io.format("****Sensing****:~nSignal from the environment~p~n", [sensory_signal])
+        n_pid <- {self(), :forward, sensory_signal}
+        sensor(n_pid)
+      :terminate ->
+        :ok
+    end
+  end
+
+  def actuator(n_pid) do
+    receive do
+      {n_pid, :forward, control_signal} ->
+        :io.format("****Acting****:~nUsing:~p to act on environment.~n", [control_signal])
+        actuator(n_pid)
+      :terminate ->
+        :ok
+    end
+  end
+
+  def cortex(sensor_pid, neuron_pid, actuator_pid) do
+    receive do
+      :sense_think_act ->
+        sensor_pid <- :sync
+        cortex(sensor_pid, neuron_pid, actuator_pid)
+      :terminate ->
+        sensor_pid <- :terminate
+        neuron_pid <- :terminate
+        actuator_pid <- :terminate
+        :ok
+    end
+  end
+end
